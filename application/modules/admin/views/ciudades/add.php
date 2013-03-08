03acc3cfa6667d9a7f14f0d8c28f4c7bfe7db820
Daniel Bertoni <daniel.bertoni@gmail.com>
2013-03-08 15:41:34 -0300

    se agrego el submodulo ciudades

---
+++ application/modules/admin/views/ciudades/add.php
@@ -0,0 +1,66 @@
+<?php echo form_open($accion, 'id="escuela-Form"')?>
+<table>
+  <tr>
+    <th>Nombre:</th>
+    <td><?php echo form_input('nombre', $esc->nombre, 'size="20"');?></td>
+  </tr>
+  <tr>
+    <th>Direccion:</th>
+    <td><?php echo form_input('direccion', $esc->direccion, 'size="40"');?></td>
+  </tr>
+  <tr>
+    <th>Ciudad:</th>
+    <td><?php echo form_dropdown('ciudad_id', $selCiudades,$esc->ciudad_id, '');?></td>
+  </tr>
+  <tr>
+    <th>Telefono:</th>
+    <td><?php echo form_input('telefono', $esc->telefono, 'size="20"');?></td>
+  </tr>
+  <tr>
+    <th>Director:</th>
+    <td><?php echo form_input('director', $esc->director, 'size="20"');?></td>
+  </tr>
+  <tr>
+    <th>Numero Establecimiento:</th>
+    <td><?php echo form_input('numero_estab', $esc->numero_estab, 'size="5"');?></td>
+  </tr>
+  <tr>
+    <th>lugar de retiro de Transporte:</th>
+    <td><?php echo form_textarea('lugarTransporte', $esc->lugarTransporte,'id="lugartransporte"');?></td>
+  </tr>  
+  <tr>
+    <td colspan="2"><div id="Guardar" class="boton"><?php echo $botGuardar?></div><div id="Cancelar" class="boton">Cancelar</div></td>
+  </tr>
+  <tr>
+    <td colspan="2"><?php echo form_hidden('programa_id', $esc->programa_id, '');?></td>
+    <td colspan="2"><?php echo form_hidden('id', $esc->id, '');?></td>
+  </tr>
+</table>
+<?php echo form_close();?>
+
+
+<script>
+$(document).ready(function(){
+  $('input').css('float','left');
+  $("#lugartransporte").css('width','250');
+  $(".boton").button();
+  var options = {
+      //target:     '#divToUpdate',
+      //url:        'comment.php',
+      success:    function() {
+        var nombre= $('#escuela-Form').parent().attr('id');
+        nombre = "#" + nombre;
+        $(nombre).dialog('destroy');
+        location.reload();
+      }
+  };
+  $("#escuela-Form").ajaxForm(options);
+  $("#Guardar").click(function(){
+    $("#escuela-Form").submit();
+  });
+  $("#Cancelar").click(function(){
+    var nombre = "#" + $(this).parent().attr('id');
+    $(nombre).dialog("close");
+  });
+});
+</script>
